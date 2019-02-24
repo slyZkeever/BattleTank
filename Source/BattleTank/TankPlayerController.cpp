1aba7064e661a2669e674dc1358994579a68aa29
Vikramaditya Thakur <singh.vikramsingh.vikram92@gmail.com>
2019-02-25 03:41:50 +0530

    found location of crosshair on screen.

--- Source/BattleTank/TankPlayerController.cpp
+++ Source/BattleTank/TankPlayerController.cpp
@@ -26,6 +26,8 @@ void ATankPlayerController::Tick(float DeltaTime)
 	//UE_LOG(LogTemp, Warning, TEXT("Tick working"));
 }
 
+
+
 ATank* ATankPlayerController::GetTankController() const
 {
 	return Cast<ATank>(GetPawn());
@@ -34,7 +36,33 @@ ATank* ATankPlayerController::GetTankController() const
 void ATankPlayerController::AimAtCrosshair()
 {
 	if (!GetTankController()) { return; }
-	//Get world location through line tracing at crosshair
-	    //if anything hit 
-	        //tell controlled tank to aim at this location    
+
+	FVector HitLocation;
+	
+	       
+	if (GetSightRayHitLocation(HitLocation)) 
+	{
+		//UE_LOG(LogTemp, Warning, TEXT("HitLocation: %s"), *HitLocation.ToString());
+		
+		//TODO tell controlled tank to aim at this location 
+	}
+	
+}
+
+
+//Get world location of line tracing hit object through crosshair, return true if hit landscape
+bool ATankPlayerController::GetSightRayHitLocation(FVector& HitLocation) const
+{
+	//get the location of crosshair 
+	int32 ViewportSizeX, ViewportSizeY;
+	GetViewportSize(ViewportSizeX, ViewportSizeY);
+
+	FVector2D ScreenLocation = FVector2D(ViewportSizeX*CrosshairXLocation, ViewportSizeY*CrosshairYLocation);
+
+	//UE_LOG(LogTemp, Warning, TEXT("Crosshair Location: %s"), *ScreenLocation.ToString());
+
+
+	//pass the location of crosshair to linetracer 
+	//line trace to the provided location 
+	return true;
 }
--- Source/BattleTank/TankPlayerController.h
+++ Source/BattleTank/TankPlayerController.h
@@ -16,15 +16,26 @@ class BATTLETANK_API ATankPlayerController : public APlayerController
 	GENERATED_BODY()
 public:
 	
-	ATank* GetTankController() const;
-
 	virtual void Tick(float DeltaTime) override;
 
 protected:
 	virtual void BeginPlay() override;
 
-private:
-	//aim turret at this location to fire
-	void AimAtCrosshair();	
+private: //for functions
+
+	
+	void AimAtCrosshair();	//aim turret at this location to fire
+
+	ATank* GetTankController() const;
+	
+	bool GetSightRayHitLocation(FVector& HitLocation) const;
+
+private: //for variables 
+
+	UPROPERTY(EditAnywhere)
+	float CrosshairXLocation = 0.5f;
+
+	UPROPERTY(EditAnywhere)
+	float CrosshairYLocation = 0.5f;
 	
 };
